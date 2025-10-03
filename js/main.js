d174da77da6942da524d04095128b86036139f7d
Amani Albarazi <aalbarazi@effat.edu.sa>
2025-10-03 16:20:14 +0300

    main.js

--- js/main.js
+++ js/main.js
@@ -1,28 +1,3 @@
-"use strict";
-import form from "./form.js";
-import skillbar from "./skillbar.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-  AOS.init({
-    once: true,
-  });
-  form();
-  skillbar();
-
-  const nav = document.querySelector("#nav");
-  const navBtn = document.querySelector("#nav-btn");
-  const navBtnImg = document.querySelector("#nav-btn-img");
-
-  //Hamburger menu
-  navBtn.onclick = () => {
-    if (nav.classList.toggle("open")) {
-      navBtnImg.src = "img/icons/close.svg";
-    } else {
-      navBtnImg.src = "img/icons/open.svg";
-    }
-  };
-
-
 
 "use strict";
 import form from "./form.js";
